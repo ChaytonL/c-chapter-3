5f658f6d6614a8697d7d8f181ac8201ecaa81ada
ChaytonL <chaytonlofgren@gmail.com>
2018-08-30 11:12:54 -0500

    Hotel Stay, The Juice Finished

---
+++ HotelStay/HotelStay/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	double DaysStayed;
+	double RoomCharge;
+	double InternetCharge;
+	double TotalBill;
+
+
+	cout << " What is your name? ";
+	cin >> name;
+
+	cout << " How many days will you be staying with us? ";
+	cin >> DaysStayed;
+
+	RoomCharge = 120 * DaysStayed;
+	InternetCharge = 9.99 * DaysStayed;
+	TotalBill = RoomCharge + InternetCharge;
+
+	cout << " Yeahhhhhhhhhh Boyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy " << endl;
+
+	cout << " Your total will be $" << TotalBill << " for staying with us for " << DaysStayed << " nights "  << endl;
+
+	cout << " Yeahhhhhhhhhh Boyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy " << endl;
+
+
+	system("pause");
+	return 0;
+
+}
